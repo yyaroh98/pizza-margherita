66bd12e5f3956f48e7e002f76673da090e2c80ac
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-05 13:47:04 +0300

    add BaseTest

---
+++ src/test/java/by/pizzatempo/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package by.pizzatempo.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://www.pizzatempo.by/");
+    }
+
+    @AfterMethod
+    void tearDown() {
+        driver.quit();
+    }
+}
