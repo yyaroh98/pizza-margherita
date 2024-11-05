a658539b479804a0897833b5c557d7520a68705c
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-05 13:46:16 +0300

    add MenuMessage

---
+++ src/test/java/by/pizzatempo/pages/MenuMessage.java
@@ -0,0 +1,6 @@
+package by.pizzatempo.pages;
+
+public class MenuMessage {
+    public static final String TEXT_PIZZA_MARGARITA_IN_BASKET = "Пицца \"Маргарита\" (21см)";
+    public static final String TEXT_DRINKS_KVAS_IN_BASKET = "Квас Аливарский Dark Side,1 литр";
+}
