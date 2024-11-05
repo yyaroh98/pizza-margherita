38c401c468e1421fb1df3046ef10fe3baf5921cc
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-05 13:45:55 +0300

    add Xpath

---
+++ src/test/java/by/pizzatempo/pages/MenuXpath.java
@@ -0,0 +1,13 @@
+package by.pizzatempo.pages;
+
+public class MenuXpath {
+    public static final String BUTTON_MENU_PIZZA = "//a[@href='https://www.pizzatempo.by/menu/pizza.html']";
+    public static final String BUTTON_ADD_TO_BASKET_PIZZA_MARGARITA = "//*[@id=\"1\"]/div/div[2]/div[3]/div[3]/button/span/span";
+    public static final String BUTTON_CONFIRM_ADD_TO_BASKET_PIZZA_MARGARITA = "//*[@id=\"order_pizza_popup\"]/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]/button";
+    public static final String BUTTON_BASKET = "//div[@class='basket-top']//a[@href='https://www.pizzatempo.by/menu/order/']";
+    public static final String BUTTON_MENU_DRINKS = "/html/body/div/div[3]/ul/li[6]/a";
+    public static final String BUTTON_ADD_TO_BASKET_DRINKS = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[2]/div/div[2]/div[2]/button";
+    public static final String TEXT_DRINKS_KVAS = "//*[@id=\"basket_expand\"]/table/tbody/tr[2]/td[2]/div[1]";
+    public static final String TEXT_PIZZA_MARGARITA = "//*[@id=\"basket_expand\"]/table/tbody/tr[1]/td[2]/div[1]";
+}
+
